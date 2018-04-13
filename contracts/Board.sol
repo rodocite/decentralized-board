3888336a94479c5631cf824cd1c579c4a84b15a3
Rodolfo Yabut <rodolfoyabut@gmail.com>
2018-04-12 19:56:30 -0700

    Board and Storage Smart Contracts basic implementation

--- contracts/Board.sol
+++ contracts/Board.sol
@@ -3,15 +3,21 @@ pragma solidity ^0.4.18;
 import "./Storage.sol";
 import "./Bank.sol";
 
-contract Scaffold {
+contract Board {
   Storage messageStorage;
   Bank bank;
 
-  function Todo(address _bank, address _storage) public {
-    bank = Bank(_bank);
+  function Board(address _storage, address _bank) public {
     messageStorage = Storage(_storage);
+    bank = Bank(_bank);
   }
 
+  function setAddress(bytes32 ipfsAddress) public payable {
+    messageStorage.set(ipfsAddress);
+  }
 
+  function getAddress() public view returns (bytes32) {
+      return messageStorage.getAddress();
+  }
 }
 
--- contracts/Storage.sol
+++ contracts/Storage.sol
@@ -1,13 +1,13 @@
 pragma solidity ^0.4.18;
 
 contract Storage {
-  string public ipfsAddress_;
+  bytes32 public ipfsAddress_;
 
-  function getByAddress() public view returns (string) {
-    return ipfsAddress_;
+  function set(bytes32 ipfsAddress) public {
+    ipfsAddress_ = ipfsAddress;
   }
 
-  function insert(string ipfsAddress) public {
-    ipfsAddress_ = ipfsAddress;
+  function getAddress() public view returns (bytes32) {
+    return ipfsAddress_;
   }
 }
