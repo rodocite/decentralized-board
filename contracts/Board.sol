766bcdd5008ef523eefe5c2947da68233ede8213
Rodolfo Yabut <rodolfoyabut@gmail.com>
2018-04-17 23:39:41 -0700

    Refactor to use inheritance
    Inheritance allows Board to use `getAddresses` directly-- avoiding dynamic type errors.

--- contracts/Board.sol
+++ contracts/Board.sol
@@ -1,23 +1,7 @@
-pragma solidity ^0.4.18;
+pragma solidity ^0.4.20;
 
 import "./Storage.sol";
 
-contract Board {
-  Storage messageStorage;
-  address owner;
+contract Board is Storage(msg.sender) {
 
-  function Board(address _storage) public {
-    owner = msg.sender;
-    messageStorage = Storage(_storage);
-  }
-
-  function setAddress(bytes32 ipfsAddress) public payable {
-    require(msg.value > 0 ether);
-    messageStorage.set(ipfsAddress, msg.value);
-    owner.transfer(msg.value);
-  }
-
-  function getAddress() public view returns (bytes32[], uint[]) {
-    return messageStorage.getAddresses();
-  }
 }
--- contracts/Storage.sol
+++ contracts/Storage.sol
@@ -1,6 +1,12 @@
-pragma solidity ^0.4.18;
+pragma solidity ^0.4.20;
 
 contract Storage {
+  address owner;
+
+  function Storage(address owner_) public {
+    owner = owner_;
+  }
+
   struct VirtualEstate {
     bytes32 ipfsAddress;
 	  uint value;
@@ -8,17 +14,19 @@ contract Storage {
 
   VirtualEstate[] public estates;
 
-  function set(bytes32 ipfsAddress, uint value) public {
-    estates.push(VirtualEstate(ipfsAddress, value));
+  function set(bytes32 ipfsAddress) public payable {
+    require(msg.value > 0 ether);
+    estates.push(VirtualEstate(ipfsAddress, msg.value));
+    owner.transfer(msg.value);
   }
 
-  function getAddresses() view external returns (bytes32[], uint[]) {
+  function getAddresses() view public returns (bytes32[], uint[]) {
     bytes32[] memory addresses = new bytes32[](estates.length);
     uint[] memory values = new uint[](estates.length);
 
     for(uint i = 0; i < estates.length; i++) {
-      addresses[i] = estates[i].ipfsAddress;
-      values[i] = estates[i].value;
+        addresses[i] = estates[i].ipfsAddress;
+        values[i] = estates[i].value;
     }
 
     return (addresses, values);
