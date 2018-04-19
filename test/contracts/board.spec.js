ab87101ca9382657be8693f056e081c247f52515
Rodolfo Yabut <rodolfoyabut@gmail.com>
2018-04-19 15:27:51 -0700

    Create new tests
    Update tests and make them pass

--- test/contracts/board.spec.js
+++ test/contracts/board.spec.js
@@ -2,6 +2,7 @@ const Web3 = require('web3')
 const decode = require('abi-decoder')
 const compiledContracts = require('../../contracts/contracts.json')
 const deployedAddresses = require('../../contracts/addresses.json')
+const _ = require('lodash')
 
 describe('Board :: Contracts', () => {
   let Board
@@ -12,13 +13,27 @@ describe('Board :: Contracts', () => {
     Board = new web3.eth.Contract(compiledContracts.Board.abi, deployedAddresses.Board)
   })
 
-  describe('Board.setAddress, Board.getAddress', () => {
+  describe('Board.setAddress', () => {
     it('should set the ipfs address', async () => {
       const hash = '0x88afe29d4e57373e6ba24bb8bb93d680d57f2be7faebec05fda56584edd4934c'
-      Board.methods.setAddress(hash).send({ from: accounts[0] })
-      const getValue = await Board.methods.getAddress().call({ from: accounts[0] })
+      const setValue = await Board.methods.setAddress(hash).send({
+        from: accounts[1],
+        value: 1,
+        gas: 2000000
+      })
 
-      expect(getValue).toEqual(hash)
+      expect(setValue).toBeTruthy()
+    })
+  })
+
+  describe('Board.getAddresses', () => {
+    it('should return tuples of addresses and values', async () => {
+      const hash = '0x88afe29d4e57373e6ba24bb8bb93d680d57f2be7faebec05fda56584edd4934c'
+      const expected = {"0": ["0x88afe29d4e57373e6ba24bb8bb93d680d57f2be7faebec05fda56584edd4934c"], "1": ["1"]}
+
+      const getValue = await Board.methods.getAddresses().call()
+
+      expect(getValue).toEqual(expected)
     })
   })
 })
